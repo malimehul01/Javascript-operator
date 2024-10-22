ba6cff9a2fe739a469077caef7fff00e9a186646
mali mehul <malimehul047@gmail.com>
2024-10-23 00:10:17 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,47 @@
+//Arithmatic operators
+let a=10;
+let b=3;
+console.log("addition",a+b);
+console.log("subtraction",a-b);
+console.log("multi",a*b);
+console.log("divison",a/b);
+console.log("modulus",a%b);
+console.log("expontiation",a**b);
+
+
+
+
+//increment and decrement 
+
+let c=10;
+console.log("a",--c);
+console.log("a",++c);
+//asingment operator
+
+let j=10;
+console.log("j",j+=4);
+console.log("j",j*=4);
+console.log("j",j-=4);
+console.log("j",j/=4);
+
+//comparison operator
+ 
+let m=5;
+let l=5;
+
+console.log("m==l",m==l);
+console.log("m!=l",m!=l);
+console.log("m>l",m>l);
+console.log("m<l",m<l);
+console.log("m=<l",m<=l);
+console.log("m=>l",m>=l);
+
+
+//logical operator
+
+let condi1=10;
+let condi2=12;
+console.log("condition",condi1!=condi2 &&  condi1 <condi2);
+console.log("condition",condi1==condi2 || condi1 <condi2);
+console.log("condition",!(condi1!=condi2));
+
